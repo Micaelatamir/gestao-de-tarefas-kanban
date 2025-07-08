dccaa4aeba9f6a6b1e2ed0de9c16f44113311e77
Micaela Tamires <micaela.tamir@gmail.com>
2025-07-08 10:40:28 -0300

    update na Tarefa.controller

--- src/main/java/com/micaela/kanban/controller/TarefaController.java
+++ src/main/java/com/micaela/kanban/controller/TarefaController.java
@@ -2,32 +2,27 @@ package com.micaela.kanban.controller;
 
 import com.micaela.kanban.model.Tarefa;
 import com.micaela.kanban.repository.TarefaRepository;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-
+import java.util.List;
 
 @RestController
 @RequestMapping("/tarefas")
 public class TarefaController {
+
     private final TarefaRepository repo;
 
-    public
-    TarefaController(TarefaRepository repo){
+    public TarefaController(TarefaRepository repo) {
         this.repo = repo;
     }
 
     @PostMapping
-    public Tarefa criar(@RequestBody Tarefa tarefa){
-       return repo.save(tarefa);
-
+    public Tarefa criar(@RequestBody Tarefa tarefa) {
+        return repo.save(tarefa);
     }
-    
 
-
-
-
+    @GetMapping
+    public List<Tarefa> listarTodas() {
+        return repo.findAll();
+    }
 }
